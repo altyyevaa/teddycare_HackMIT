608debdae2170396c1feeb17c2982f0fa8594984
mahdi afshari <mahdiafshari.1300@gmail.com>
2024-09-14 19:43:55 -0400

    removed om key

--- config.py
+++ config.py
@@ -7,11 +7,6 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
-OPENAI_API_KEY = os.environ.get(
-    "OPENAI_API_KEY",
-    "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",
-)
-
 TERRA_DEV_ID = os.environ.get("TERRA_DEV_ID", "4actk-swing-testing-WwNOgPcxJ8")
 TERRA_API_KEY = os.environ.get("TERRA_API_KEY", "bYY_oG_vvUo5bK8LUgg5N47dcmg2zjk_")
 TERRA_SECRET = os.environ.get("TERRA_SECRET", "your-terra-secret")
